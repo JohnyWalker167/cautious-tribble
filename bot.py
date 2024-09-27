9e03dda59c153186daf96e30fbe55fa0941f02f6
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-28 01:11:40 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,149 @@
+import os 
+import time
+import queue
+import asyncio
+from utils import *
+from config import *
+from html import escape
+from pyrogram import idle
+from pyromod import listen
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+from asyncio import get_event_loop
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from status import *
+
+DOWNLOAD_PATH = "downloads/"
+loop = get_event_loop()
+
+os.makedirs(DOWNLOAD_PATH, exist_ok=True)
+
+user_data = {}
+TOKEN_TIMEOUT = 7200
+
+# Create a global task queue
+task_queue = queue.Queue()
+initial_messages = {}  # Store initial messages for progress updates
+
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH, 
+    bot_token=BOT_TOKEN, 
+    workers=1000, 
+    parse_mode=enums.ParseMode.HTML
+)
+
+async def worker():
+    while True:
+        message = await loop.run_in_executor(None, task_queue.get)
+        if message is None:
+            break  # Exit if a sentinel value is received
+        
+        await forward_message_to_new_channel(app, message)
+        task_queue.task_done()
+
+async def main():
+    # Start the worker
+    loop.create_task(worker())
+    
+    async with app:
+        await idle()
+
+with app:
+    bot_username = (app.get_me()).username
+
+@app.on_message(filters.private & (filters.document | filters.video))
+async def enqueue_message(client, message):
+    # Send an initial message when a new task is added to the queue
+    initial_msg = await message.reply_text("📥 Preparing to download your file...")
+
+    # Add the message and the initial message reference to the task queue
+    task_queue.put((message, initial_msg))
+    initial_messages[message.id] = initial_msg  # Store reference for later updates
+
+async def forward_message_to_new_channel(client, message_tuple):
+    user_id = message_tuple.from_user.id
+    try:
+        message, initial_msg = message_tuple  # Unpack the tuple
+        media = message.document or message.video
+        file_id = message.id
+
+        if media:
+            caption = message.caption if message.caption else None
+
+            if caption:
+                new_caption = await remove_unwanted(caption)
+                cap_no_ext = await remove_extension(new_caption)
+
+                logger.info(f"Downloading initial part of {file_id}...")
+
+                reset_progress()
+                file_path = await app.download_media(message, file_name=f"{new_caption}", 
+                                                     progress=progress,
+                                                     progress_args=("Download", initial_msg)
+                                                     )
+
+                # Generating Thumbnail
+                movie_name, release_year = await extract_movie_info(cap_no_ext)
+                thumbnail_path = await get_movie_poster(movie_name, release_year)
+                duration = await generate_duration(file_path)
+
+                if thumbnail_path:
+                    logger.info(f"Thumbnail generated: {thumbnail_path}")
+                else:
+                    logger.info("Failed to generate thumbnail")
+
+                upld_msg = await initial_msg.edit_text("⏫ Uploading")
+
+                reset_progress()
+                await app.send_video(
+                    user_id,
+                    video=file_path,
+                    caption=f"<code>{escape(new_caption)}</code>",
+                    duration=duration,
+                    width=480,
+                    height=320,
+                    thumb=thumbnail_path,
+                    progress=progress,
+                    progress_args=("Upload", upld_msg)
+                )
+
+                await upld_msg.edit_text("Uploaded ✅")
+
+                os.remove(thumbnail_path)
+                os.remove(file_path)
+
+                await asyncio.sleep(3)
+
+    except Exception as e:
+        logger.error(f'{e}') 
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        if os.path.exists(thumbnail_path):
+            os.remove(thumbnail_path)
+        if message.id in initial_messages:
+            del initial_messages[message.id]  # Clean up the initial message reference
+
+@app.on_message(filters.command("start"))
+async def get_command(client, message):
+    reply = await message.reply_text(f"<b>💐Welcome this is TG⚡️Flix Bot")
+    await auto_delete_message(message, reply)
+
+# Get Log Command
+@app.on_message(filters.command("log") & filters.user(OWNER_USERNAME))
+async def log_command(client, message):
+    user_id = message.from_user.id
+
+    # Send the log file
+    try:
+        reply = await app.send_document(user_id, document=LOG_FILE_NAME, caption="Bot Log File")
+        await auto_delete_message(message, reply)
+    except Exception as e:
+        await app.send_message(user_id, f"Failed to send log file. Error: {str(e)}")
+    
+if __name__ == "__main__":
+    logger.info("Bot is starting...")
+    loop.run_until_complete(main())
+    logger.info("Bot has stopped.")
---
+++ utils.py
@@ -0,0 +1,200 @@
+import re
+import aiohttp
+import subprocess
+import asyncio
+from config import *
+
+async def auto_delete_message(user_message, bot_message):
+    try:
+        await user_message.delete()
+        await asyncio.sleep(60)
+        await bot_message.delete()
+    except Exception as e:
+        logger.error(f"{e}")
+        
+def get_readable_time(seconds: int) -> str:
+    result = ""
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f"{days}d"
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f"{hours}h"
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f" {minutes}m"
+    seconds = int(seconds)
+    result += f" {seconds}s"
+    return result
+    
+async def remove_extension(caption):
+    try:
+        # Remove .mkv and .mp4 extensions if present
+        cleaned_caption = re.sub(r'\.mkv|\.mp4|\.webm', '', caption)
+        return cleaned_caption
+    except Exception as e:
+        logger.error(e)
+        return None
+
+async def remove_unwanted(input_string):
+    # Use regex to match .mkv or .mp4 and everything that follows
+    result = re.split(r'(\.mkv|\.mp4)', input_string)
+    # Join the first two parts to get the string up to the extension
+    return ''.join(result[:2])
+
+def humanbytes(size):
+    # Function to format file size in a human-readable format
+    if not size:
+        return "0 B"
+    # Define byte sizes
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    i = 0
+    while size >= 1024 and i < len(suffixes) - 1:
+        size /= 1024
+        i += 1
+    f = ('%.2f' % size).rstrip('0').rstrip('.')
+    return f"{f} {suffixes[i]}"
+
+  
+async def extract_tg_link(telegram_link):
+    try:
+        pattern = re.compile(r'https://t\.me/c/(-?\d+)/(\d+)')
+        match = pattern.match(telegram_link)
+        if match:
+            message_id = match.group(2)
+            return message_id
+        else:
+            return None, None
+    except Exception as e:
+        logger.error(e)
+
+async def extract_channel_id(telegram_link):
+    try:
+        pattern = re.compile(r'https://t\.me/c/(-?\d+)/(\d+)')
+        match = pattern.match(telegram_link)
+        if match:
+            channel_id = match.group(1)
+            formatted_channel_id = f'-100{channel_id}'
+            return formatted_channel_id
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        
+'''
+async def download_initial_part(client, media, file_path, chunk_size):
+    # Open the file for writing in binary mode
+    with open(file_path, 'wb') as f:
+        async for chunk in client.stream_media(media):
+            f.write(chunk)
+            if f.tell() >= chunk_size:
+                break
+'''
+
+async def extract_movie_info(caption):
+    try:
+        regex = re.compile(r'(.+?)(\d{4})')
+        match = regex.search(caption)
+
+        if match:
+            # Replace '.' and remove '(' and ')' from movie_name
+            movie_name = match.group(1).replace('.', ' ').replace('(', '').replace(')', '').strip()
+            release_year = match.group(2)
+            return movie_name, release_year
+    except Exception as e:
+        print(e)
+    return None, None
+
+
+async def generate_duration(file_path: str) -> str:
+    try:
+
+        # Use ffprobe to get video duration
+        duration_cmd = [
+            'ffprobe', '-v', 'error', '-show_entries', 'format=duration', 
+            '-of', 'default=noprint_wrappers=1:nokey=1', file_path
+        ]
+        duration = float(subprocess.check_output(duration_cmd).strip())
+
+        return duration
+    except Exception as e:
+        logger.error(f"Error generating duration: {e}")
+        return None
+    
+async def get_movie_poster(movie_name, release_year):
+    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(tmdb_search_url) as search_response:
+                search_data = await search_response.json()
+
+                if search_data['results']:
+                    # Filter results based on release year and first air date
+                    matching_results = [
+                        result for result in search_data['results']
+                        if ('release_date' in result and result['release_date'][:4] == str(release_year)) or
+                        ('first_air_date' in result and result['first_air_date'][:4] == str(release_year))
+                    ]
+
+                    if matching_results:
+                        result = matching_results[0]
+
+                        # Fetch additional details using movie ID
+                        movie_id = result['id']
+                        media_type = result['media_type']
+
+                        tmdb_movie_image_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en,hi'
+
+                        async with session.get(tmdb_movie_image_url) as movie_response:
+                            movie_images = await movie_response.json()
+ 
+                        # Use the backdrop_path or poster_path
+                            poster_path = None
+                            if 'backdrops' in movie_images and movie_images['backdrops']:
+                                poster_path = movie_images['backdrops'][0]['file_path']
+                                                         
+                            elif 'posters' in movie_images and movie_images['posters']:
+                                poster_path = movie_images['posters'][0]['file_path']
+
+                            if poster_path:
+                                poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                    
+                                async with session.get(poster_url) as image_response:
+                                    if image_response.status == 200:
+                                        # Define the local file path to save the poster
+                                        file_name = f"{movie_name.replace(' ', '_')}_{release_year}.jpg"
+                                        file_path = os.path.join("downloads/", file_name)
+
+                                        with open(file_path, 'wb') as f:
+                                            f.write(await image_response.read())
+
+                                        # Return the local file path after downloading
+                                        return file_path
+                                   
+                        
+    except Exception as e:               
+        logger.error(f"Error fetching TMDB data: {e}")
+
+    return None
+
+'''
+def generate_thumbnail(file_path: str) -> str:
+    try:
+        # Output thumbnail path
+        thumbnail_path = f"{file_path}.jpg"
+
+        # Use ffmpeg to generate a thumbnail
+        (
+            zender
+            .input(file_path, ss='00:00:01')  # Seek to 1 second
+            .output(thumbnail_path, vframes=1)
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+        return thumbnail_path
+    except Exception as e:
+        print(f"Error generating thumbnail: {e}")
+        return None
+'''
